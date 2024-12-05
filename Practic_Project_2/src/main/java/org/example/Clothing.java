0cda481d326ac6e720a317c84f1b647a86b77863
vovan22 <65122611+vovan22@users.noreply.github.com>
2024-12-05 11:42:51 +0300

    lab2

---
+++ Practic_Project_2/src/main/java/org/example/Clothing.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Clothing extends Product {
+    public Clothing(String name) {
+        super(name);
+    }
+}
---
+++ Practic_Project_2/src/main/java/org/example/Product.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+    private String name;
+}
